ce66689aef5f63a5a3c0a6a262fe2118acd41795
Keerat <itskeeratkaur@gmail.com>
2025-08-06 01:27:13 +0530

    Initial commit - portfolio base with Spline setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Spline from '@splinetool/react-spline';
+
+function App() {
+  return (
+    <div style={{ height: '100vh', width: '100vw' }}>
+      <Spline scene="https://prod.spline.design/qI5jHGP3lBYBbWFt/scene.splinecode" />
+    </div>
+  );
+}
+
+export default App;
